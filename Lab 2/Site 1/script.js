af41bb642e819ef08d1a440c725d499815984820
Yuliia Bulakh <107890026+CryptoBeeJulia@users.noreply.github.com>
2024-02-22 19:51:46 +0200

    end-of-class-commit
    js applied

---
+++ Lab 2/Site 1/script.js
@@ -0,0 +1,24 @@
+let activity = document.querySelector("#activity");
+let participants = document.querySelector("#participants");
+let price = document.querySelector("#price");
+let type = document.querySelector("#type");
+
+const url = 'https://www.boredapi.com/api/activity';
+const newText = document.getElementById("new");
+
+async function getData()
+{
+    const resp = await fetch(url);
+    //console.log(resp);
+    const respData = await resp.json();
+    console.log(respData);
+    console.log(respData.activity);
+
+    activity.innerHTML = respData.activity;
+    participants.innerHTML = respData.participants;
+    price.innerHTML = respData.price;
+    type.innerHTML = respData.type;
+}
+getData();
+
+
