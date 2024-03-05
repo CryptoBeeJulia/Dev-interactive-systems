fa3d271cf3a05289ab67c821c7b041f02f6096c8
Yuliia Bulakh <107890026+CryptoBeeJulia@users.noreply.github.com>
2024-03-05 10:45:09 +0200

    app1 fixing attempt
    terminal stopped responding

--- Lab4/app1/Hello.js
+++ Lab4/app1/Hello.js
@@ -1,3 +1,15 @@
+
+/*
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.js';
+
+ReactDOM.createRoot(
+    document.querySelector('#root')
+    ).render(<App/>) 
+*/
+
 'use strict';
 
 class Hello extends React.Component { 
