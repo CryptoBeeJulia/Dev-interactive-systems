0b4111c9c924e3e4e296a15fd6e75ba400c144ca
Yuliia Bulakh <107890026+CryptoBeeJulia@users.noreply.github.com>
2024-03-20 12:06:35 +0200

    step 1
    square rendered and working

--- Homework/home1/src/App.js
+++ Homework/home1/src/App.js
@@ -14,103 +14,43 @@ function App() {
 
 // note! if the below function works, it replaces the Hello function!
 
-function Square({value, onClick})
+function Square({value})
 {
   return (
-    <button className = "square" onClick={onClick}>
+    <button className = "square">
     {value}
     </button>
     )
   }
 
-  function Board () {
-    const [squares, setSquares] = React.useState(Array(9).fill(null))
-    const [isX, setIsX] = React.useState(true)
 
-    const handleClick = (i) => {
-      squares[i] = isX? 'X': 'O' 
-      setSquares(squares)
-      setIsX(!isX)
-    }
-
-    return(
-      <div className = "board">
-        <div className = "boardRow">
-        <Square value={squares[0]} onClick={() => handleClick(0)}/>
-        <Square value={squares[1]} onClick={() => handleClick(1)}/>
-        <Square value={squares[2]} onClick={() => handleClick(2)}/>
-        </div>
-        <div className = "boardRow">
-        <Square value={squares[3]} onClick={() => handleClick(3)}/>
-        <Square value={squares[4]} onClick={() => handleClick(4)}/>
-        <Square value={squares[5]} onClick={() => handleClick(5)}/>
-        </div>
-        <div className = "boardRow">
-        <Square value={squares[6]} onClick={() => handleClick(6)}/>
-        <Square value={squares[7]} onClick={() => handleClick(7)}/>
-        <Square value={squares[8]} onClick={() => handleClick(8)}/>
-        </div>
-      </div>
-    )
-  }
 
   ReactDOM.render(
-    <Board/>, 
+    <Square value={'X'}/>, 
     document.getElementById('root')
   )
 
 
 export default App;
 
+/*
 
+step 1: square rendered and working
 
- /*
- function Square({value, onClick})
+function Square({value})
 {
   return (
-    <button className = "square" onClick={onClick}>
+    <button className = "square">
     {value}
     </button>
     )
   }
 
-  function Board () {
-    const [squares, setSquares] = React.useState(Array(9).fill(null))
-    const [isX, setIsX] = React.useState(true)
 
-    const handleClick = (i) => {
-      squares[i] = isX? 'X': 'O' 
-      setSquares(squares)
-      setIsX(!isX)
-    }
-
-    return(
-      <div className = "board">
-        <div className = "boardRow">
-        <Square value={squares[0]} onClick={() => handleClick(0)}/>
-        <Square value={squares[1]} onClick={() => handleClick(1)}/>
-        <Square value={squares[2]} onClick={() => handleClick(2)}/>
-        </div>
-        <div className = "boardRow">
-        <Square value={squares[3]} onClick={() => handleClick(3)}/>
-        <Square value={squares[4]} onClick={() => handleClick(4)}/>
-        <Square value={squares[5]} onClick={() => handleClick(5)}/>
-        </div>
-        <div className = "boardRow">
-        <Square value={squares[6]} onClick={() => handleClick(6)}/>
-        <Square value={squares[7]} onClick={() => handleClick(7)}/>
-        <Square value={squares[8]} onClick={() => handleClick(8)}/>
-        </div>
-      </div>
-    )
-  }
 
   ReactDOM.render(
-    <Board/>, 
+    <Square value={'X'}/>, 
     document.getElementById('root')
   )
 
-  */
-  
-
-  
+*/
