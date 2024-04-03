905cd2228e2ed82796d798298dbee8ed596e7281
Yuliia Bulakh <107890026+CryptoBeeJulia@users.noreply.github.com>
2024-04-03 11:05:08 +0300

    Game 1
    // previously under app 2 this application saved under Game1

---
+++ Game1/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Board from './Board';
+
+function App() {
+  return (
+    <>
+    <Board/>
+    </>
+  );
+}
+
+export default App;
+
+
+// previously under app 2 this application saved under Game1
