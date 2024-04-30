7926637b50918b59c32c80544132835bef509b6d
Yuliia Bulakh <107890026+CryptoBeeJulia@users.noreply.github.com>
2024-04-30 09:19:52 +0300

    yuliiaproject
    project folder + initial app crytpoapp created with react

---
+++ yuliiaproject/cryptoapp/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>cryptoapp</h1>
+
+        </div>
+    );
+
+}
+
+export default App;
