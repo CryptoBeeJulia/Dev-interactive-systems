cb749fcefe28ff3d72207704a691ebe9f87c05f3
Yuliia Bulakh <107890026+CryptoBeeJulia@users.noreply.github.com>
2024-05-02 19:59:56 +0300

    lab 7
    lab 7 search with errors, css with errors

--- Lab7/app1/src/App.js
+++ Lab7/app1/src/App.js
@@ -2,19 +2,23 @@ import { useState, useEffect } from 'react';
 import './App.css';
 import APIKey from './config';
 import Movie from './components/Movie';
+//import "./Movie.css";
+
 
 const APIURL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=';
+const SearchURL = 'https://api.themoviedb.org/3/search/movie?api_key=';
 
 
 function App() {
 const [movieResults, setMovieResults] = useState([]);
+const [searchTerm, setSearchTerm] = useState('');
 //const movieResults = [1,2,3];
 
 useEffect( () => {
   fetch(APIURL + APIKey)
   .then( (response) => response.json() )
   .then( (data) => {
-    console.log(data);
+    console.log(data.results);
     setMovieResults(data.results);
   }
   )
@@ -22,11 +26,38 @@ useEffect( () => {
 
 }
 ,[])
+
+const handleOnSubmit = (e) => {
+  e.preventDefault();
+
+  if(searchTerm)
+  {fetch(SearchURL + APIKey + "&query=" + searchTerm)
+    .then( (response) => response.json() )
+    .then (data) => :{
+      console.log(data.results);
+      setMovieResults(data.results);}
+}
+
+const handleOnChange = (e) => {
+  console.log(e.targer.value);
+  setSearchTerm(e.target.value)
+}
   return (
     <>
-    {movieResults.map( (element, index) => (<Movie key={index}/>)
+    <form onSubmit={handleOnSubmit}>
+      <input className = "search" type = "search" placeholder = 'Search...' value = {searchTerm} onChange = {handleOnChange}></input>
+    </form>
+    <header>
+      
+    </header>
+    
+    
+    <div className = 'movie-container'>
+    {movieResults.map( (element) => (<Movie key={element.id} {...element}/>)
 
     )}
+    </div>
+
     </>
 
   );
