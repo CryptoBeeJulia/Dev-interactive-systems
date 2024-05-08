c5e0a0b8ec3d0a6ad28deb02328ff0fe5dce909c
Yuliia Bulakh <107890026+CryptoBeeJulia@users.noreply.github.com>
2024-05-08 09:18:57 +0300

    poster function done
    {poster_path ? (
                    <img src = {ImageURL + poster_path}  alt ={title}></img>
                ) : (
                    <img src = {GenericImage}  alt ={title}></img>
                    )
                }

--- Lab7/app1/src/components/Movie.js
+++ Lab7/app1/src/components/Movie.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./Movie.css";
+import GenericImage from './placeholder.jpeg';
 
 const ImageURL = 'https://image.tmdb.org/t/p/w500/';
 
@@ -8,7 +9,13 @@ function Movie({poster_path, title, overview, vote_average})
 {
     return (
         <div className = "movie">
-            <img src = {ImageURL + poster_path}  alt ={title}></img>
+            {poster_path ? (
+                <img src = {ImageURL + poster_path}  alt ={title}></img>
+            ) : ( 
+                <img src = {GenericImage}  alt ={title}></img> 
+                )
+            }
+
             <div className = "movie-info">
                 <h3>{title}</h3>
                 <span>{vote_average}</span>
