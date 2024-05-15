9c673ac8c633cdb2d016efdb2e72177e10a672fc
Yuliia Bulakh <107890026+CryptoBeeJulia@users.noreply.github.com>
2024-05-15 23:08:04 +0300

    Home, Coins pages routed

--- yuliiaproject/cryptoapp/src/App.js
+++ yuliiaproject/cryptoapp/src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 
 import React from 'react';
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
 
 import Navbar from './components/navbar';
 import Stats from './components/homepage';
@@ -9,6 +10,7 @@ import Coin from './components/coin';
 
 const App = () => {
     return (
+        <BrowserRouter>
         <div className = "app">
 
             <h3>cryptoapp</h3>
@@ -19,23 +21,17 @@ const App = () => {
 
             </div>
 
-            <div className = "main">
-        
-                <Stats/>
-
-            </div>
-
-            <div className = "coin">
-
-                <Coin/>
-
-            </div>
+            <Routes>
+        <Route path = "/" element = {<Stats/>}></Route>
+        <Route path = "/coins" element = {<Coin/>}></Route>
+        </Routes>
 
             <div className = "footer">
 
             </div>
 
         </div>
+        </BrowserRouter>
     );
 
 }
--- yuliiaproject/cryptoapp/src/components/navbar.jsx
+++ yuliiaproject/cryptoapp/src/components/navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {Link, NavLink} from 'react-router-dom';
 import cryptologo from "../components/cryptologo.jpeg";
 
 const Navbar = () => {
@@ -8,6 +9,10 @@ const Navbar = () => {
                 <img src= {cryptologo} className = "logo-img" alt = 'cryptoapp logo'></img>
                 <div className="logo-text">CryptoApp</div>
             </div>
+            <nav className = 'navigation'>
+        <NavLink to = "/" className = 'link'>Home</NavLink>
+        <NavLink to = "/coins" className = 'link'>Coins</NavLink>
+      </nav>
         </div>
     )
 }
